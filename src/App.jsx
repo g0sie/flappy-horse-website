762de789a86d0183b1cf6117f61bb29f75bc316e
g0sie <malgorzataderkacz3@gmail.com>
2024-03-24 19:52:25 +0100

    feat: add shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import PhaserGame from "./game/PhaserGame";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div id="app">
+      <Button>button</Button>
       <PhaserGame />
     </div>
   );
