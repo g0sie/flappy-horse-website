290038c224975fa23a798f679dd18a5b1c114fd1
g0sie <malgorzataderkacz3@gmail.com>
2024-04-02 22:24:15 +0200

    fix: hide firebase config in .env

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaT9camf1Kuu-uP1_95W6elriPlxmBinM",
-  authDomain: "flappy-horse-f5c1d.firebaseapp.com",
-  projectId: "flappy-horse-f5c1d",
-  storageBucket: "flappy-horse-f5c1d.appspot.com",
-  messagingSenderId: "9148462885",
-  appId: "1:9148462885:web:c463ef4e7cf95a4c830ccc",
-  measurementId: "G-7X9GJZ8ZJV",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
