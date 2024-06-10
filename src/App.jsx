616f5647f297af4768826d527523ff14882ced4c
g0sie <malgorzataderkacz3@gmail.com>
2024-06-10 21:37:50 +0200

    feat: add leaderboard page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 
 import Home from "./pages/Home/Home";
+import LeaderboardPage from "./pages/Leaderboard/Leaderboard";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/leaderboard",
+        element: <LeaderboardPage />,
       },
     ],
   },
--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 import logo from "@/assets/logo.png";
 
 const Logo = () => {
   return (
-    <div className="logo">
+    <Link to={"/"} className="logo">
       <img className="logo__img" src={logo} alt="Logo" />
       <h1 className="logo__text">flappy horse</h1>
-    </div>
+    </Link>
   );
 };
 
