9601bfd1d31d20f4b4a3a9ce4c3efd7ceecaf0b8
g0sie <malgorzataderkacz3@gmail.com>
2024-08-04 13:14:14 +0200

    feat: fetch leaderboard data

--- src/pages/Leaderboard/Leaderboard.tsx
+++ src/pages/Leaderboard/Leaderboard.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../utils/firebase.utils";
+
+interface IUser {
+  id: string;
+  displayName?: string;
+  score?: number;
+}
+
 const LeaderboardPage = () => {
-  return <p className="text-primary">haha prank</p>;
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsersFromDb = async () => {
+      try {
+        const res = await getDocs(usersCollectionRef);
+        const data: IUser[] = res.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setUsers(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUsersFromDb();
+  }, []);
+
+  return (
+    <>
+      {users.map(
+        (user) =>
+          user.displayName && (
+            <p key={user.id} className="text-primary">
+              {user.displayName} - {user.score || 0}
+            </p>
+          )
+      )}
+    </>
+  );
 };
 
 export default LeaderboardPage;
